d4329c28cc4f1ebc75d2d789fcaaf9aa3054294a
jakib01 <julkernienakib@gmail.com>
2021-08-26 17:27:04 +0600

    datatable update

--- VideoRentalStore/Context/tbl_users.cs
+++ VideoRentalStore/Context/tbl_users.cs
@@ -19,10 +19,10 @@ namespace VideoRentalStore.Context
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNo { get; set; }
-        public byte[] UserName { get; set; }
         public string Password { get; set; }
         public string Image { get; set; }
         public string Address { get; set; }
         public Nullable<int> Country { get; set; }
+        public string UserName { get; set; }
     }
 }
--- VideoRentalStore/Controllers/UserController.cs
+++ VideoRentalStore/Controllers/UserController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoRentalStore.Context;
 
 namespace VideoRentalStore.Controllers
 {
     public class UserController : Controller
     {
         // GET: User
+        db_VideoRentalstoreEntities dbObj = new db_VideoRentalstoreEntities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetData()
+        {
+            var res = dbObj.tbl_users.ToList(); //dbObj is data object 
+            return Json(new { data = res}, JsonRequestBehavior.AllowGet);
         }
     }
 }
